cbac4c1b2b536c8f3b61d4c61598e6aad6e9e7d1
pawel200020 <pawel200020@gmail.com>
2022-05-01 22:42:11 +0200

    auth with repair class

---
+++ PC-Service-Authn-APP/Authn/Models/RepairModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Authn.Models
+{
+    public class RepairModel
+    {
+        public int Id {get; set; }
+        public string Brand { get; set; }
+        public string SerialNumber { get; set; }
+        public string Condition { get; set; }
+        public string Description { get; set; }
+        public int IdOwner { get; set; }
+    }
+}
